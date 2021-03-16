8c39616bc7adf2350edee472354645012639f147
rsneal01 <rsneal01@gmail.com>
2021-03-16 11:14:02 -0700

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,19 @@
 class FiguresController < ApplicationController
-  # add controller methods
+  
+  get '/figures/new' do
+    @landmarks = Landmark.all
+    @titles = Title.all
+    # binding.pry
+    erb :'/figures/new'
+  end
+
+  post '/figures' do
+    
+    @figure = Figure.create(name: params[:figure][:name])
+    @figure.titles = Title.all.find(params[:figure][:title_ids])
+    # binding.pry
+    # @figure.landmarks = Landmark.all.find(params[:figure][:landmark_ids])
+    
+  end
+
 end
