7626b933199057d66f69dbf8e12b03433ea1b0cc
rsneal01 <rsneal01@gmail.com>
2021-03-18 12:28:24 -0700

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,5 +1,10 @@
 class FiguresController < ApplicationController
   
+  get '/figures' do
+    @figures = Figure.all
+    erb :'/figures/index'
+  end
+  
   get '/figures/new' do
     @landmarks = Landmark.all
     @titles = Title.all
@@ -7,13 +12,38 @@ class FiguresController < ApplicationController
     erb :'/figures/new'
   end
 
-  post '/figures' do
-    
-    @figure = Figure.create(name: params[:figure][:name])
-    @figure.titles = Title.all.find(params[:figure][:title_ids])
+  get '/figures/:id' do
+    @figure = Figure.find_by(params[:id])
+    erb :'/figures/show'
     # binding.pry
-    # @figure.landmarks = Landmark.all.find(params[:figure][:landmark_ids])
-    
   end
+
+  post '/figures' do
+    @figure = Figure.create(params[:figure])
+    # check to see if new Title/Figure field is not empty.  If so, create a new figure/title and associate it with the figure.
+    if params[:title][:name] != "" && params[:title][:name] != nil
+      @figure.titles << Title.create(name: params[:title][:name])
+    elsif params[:landmark][:name] != "" && params[:landmark][:name] != nil
+      @figure.landmarks << Landmark.create(name: params[:landmark][:name])
+    end
+    # might be some repetition here, need logic to check if title already exists?
+    redirect to("/figures/#{@figure.id}")
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find_by(params[:id])
+    @titles = Title.all
+    erb :'/figures/edit'
+  end
+
+  patch '/figures/:id' do
+    @figure = Figure.find_by(params[:id])
+    # binding.pry
+    @figure.name = params[:figure][:name]
+    @figure.landmarks << Landmark.create(name: params[:landmark][:name])
+    @figure.save
+    redirect to("/figures/#{@figure.id}")
+  end
+
 
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,35 @@
 class LandmarksController < ApplicationController
-  # add controller methods
+  
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'/landmarks/index'
+  end
+  
+  get '/landmarks/new' do
+    erb :'/landmarks/new'
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find_by(params[:id])
+    erb :'landmarks/show'
+  end
+
+
+  post '/landmarks' do
+    # binding.pry
+    @landmark = Landmark.create(params[:landmark])
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find_by(params[:id])
+    erb :'/landmarks/edit'
+  end
+
+  patch '/landmarks/:id' do
+    @landmark = Landmark.find_by(params[:id])
+    @landmark.update(params[:landmark])
+    redirect to("/landmarks/#{@landmark.id}")
+  end
+
+
 end
